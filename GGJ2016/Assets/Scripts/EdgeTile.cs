e1d194b6964a7701253feccd21bd392a476af39c
Tim S <MangaSketch@gmail.com>
2016-01-30 17:45:36 -0500

    Created EdgeTIle script for Dedushka and PlantTile script for Babushka. Added some trigger checks for each tile type. Tiles should be ready to receive plants/traps.

---
+++ GGJ2016/Assets/Scripts/EdgeTile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeTile : MonoBehaviour
+{
+    [SerializeField] private bool isInUse = false;
+
+    [SerializeField] private GameObject currentTrap;
+
+    public bool GetIsInUse()
+    {
+        return isInUse;
+    }
+
+    public void SetIsInUse(bool _value)
+    {
+        isInUse = _value;
+    }
+
+    public void SetCurrentPlant(GameObject _trap)
+    {
+        currentTrap = _trap;
+    }
+}
