d7e23dd09d1ea580c2b051363f33a1cc723eaa97
Tim S <MangaSketch@gmail.com>
2016-01-30 14:17:29 -0500

    Implemented the AntAI movement. Beginning work on target acquisition for Ants.

---
+++ GGJ2016/Assets/Scripts/AntAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AntAI : MonoBehaviour
+{
+    //Ant AI script
+    [SerializeField] private Transform target;
+    [SerializeField] private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void FindTarget()
+    {
+        //Pull target from game manager
+    }
+}
